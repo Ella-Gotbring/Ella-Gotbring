b7dee615df624129655440c7cd0dd4cfbe3b8d29
Ella-Gotbring <54885982+Ella-Gotbring@users.noreply.github.com>
2021-09-21 20:28:14 +0200

    update main.js
    added functionality to todo list input field

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log('hello world');
+
+let inputField = document.getElementById('inputField');
+let toDocontainer = document.getElementById('toDocontainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function() {
+    var paragraph = document.createElement('p');
+    paragraph.innerText = inputField.nodeValue;
+    toDoContainer.appendChild(paragraph);
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = "line-through";
+    })
+})
