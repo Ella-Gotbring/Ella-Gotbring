06559fc1f8dabfe2e4697f68ac5d1d730dd01285
Ella-Gotbring <54885982+Ella-Gotbring@users.noreply.github.com>
2021-09-24 12:13:28 +0200

    add if/else for loop
    added if else statement and for loop
    added comments
    changes some css

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-console.log('hello world');
+// console.log('hello world');
 
+//todo 
 let inputField = document.getElementById('inputField');
 let toDoContainer = document.getElementById('toDoContainer');
 let addToDoButton = document.getElementById('addTodo');
 
+
 addToDoButton.addEventListener('click', function() {
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
+    var paragraph = document.createElement('p'); // varje gång du trycker på knappen skapas ett p 
+    paragraph.classList.add('paragraph-styling'); //add css 
     paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
+    toDoContainer.appendChild(paragraph); //lägger till p -> container
+    inputField.value = ""; //tom string
     paragraph.addEventListener('click', function() {
         paragraph.style.textDecoration = "line-through";
     })
+    paragraph.addEventListener('dblclick', function() {
+        remove(paragraph);
+    })
 })
+
+//knapp med olika hälsningar 
+
+var d = new Date();
+console.log(d);
+
+function myTime() {
+    var time = new Date().getHours(); //getHours returnerar värde 0-23 
+    var greeting;
+    if (time < 10) {
+        greeting = "God morgon";
+    } else if (time < 16)
+        greeting = "God eftermiddag";
+    else {
+        greeting = "God kväll";
+    }
+    document.getElementById("output").innerHTML = greeting;
+}
+
+//for loop
+//variabel i = 0, condition kommer köra loopen så längre det är true 
+//lägger till 1 till i 
+function forloop() {
+    for (let i = 0; i < 5; i++) {
+        console.log('Hello');
+    }
+}
